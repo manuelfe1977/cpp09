a75216e7e51eeb3a4b03f76f97876ee89b1e611a
Manuelfe <manuelfe@student.42malaga.com>
2026-08-27 13:48:03 +0200

    ex02 compilation

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -35,14 +35,31 @@ void PmergeMe<C>::printContainer()
 }
 
 template<typename C>
-C sortFord(C &input)
+C PmergeMe<C>::_generateJacobSequence(size_t smaller_size)
+{
+	(void) smaller_size;
+	C jacobSequence;
+
+	return jacobSequence;
+}
+
+template<typename C>
+void PmergeMe<C>::_binaryInsert(C &main, int element, size_t maxSearch)
+{
+	(void) main;
+	(void) element;
+	(void) maxSearch;
+	// Lógica de búsqueda binaria...
+}
+
+template<typename C> C PmergeMe<C>::_sort(C &input)
 {
 	unsigned int	size = input.size();
 	if (size < 2)
 		return input;
 	C	smaller;
 	C	bigger;
-	for (size_t i = 0; i < size; i++)
+	for (size_t i = 0; i < size - 1; i++)
 	{
 		if (i %2 != 0)
 			continue ;
@@ -57,11 +74,24 @@ C sortFord(C &input)
 			bigger.push_back(input[i]);
 		}
 	}
-	if (size % 2 == 0)
+	if (size % 2 != 0)
 		smaller.push_back(input[size - 1]);
-	C	sorted = sortFord(bigger);
 
-	C	sequenceJacob = generateSequence<C>(smaller.size());
+	bigger.insert(bigger.begin(), smaller[0]);
+	return bigger;
+}
+
+template<typename C>
+void PmergeMe<C>::sortSequence()
+{
+	std::clock_t start = std::clock();
+
+	_container = _sort(_container);
+
+	std::clock_t end = std::clock();
+	double time = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000000.0;
+
+	std::cout << time<< std::endl;
 }
 
 template class PmergeMe<std::vector<int> >;
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -4,16 +4,21 @@
 #include <deque>
 #include <iostream>
 #include <stdlib.h>
+#include <ctime>
 
-template<typename C> class PmergeMe
+template<typename C>
+class PmergeMe
 {
 private:
-	C _container;
+	C	_container;
+	C	_generateJacobSequence(size_t smaller_size);
+	C	_sort(C &input);
+	void	_binaryInsert(C &main, int element, size_t maxSearch);
 public:
 	PmergeMe();
 	~PmergeMe();
 	void	loadArguments(char **arg);
-	template<typename C>	sortFord(C &input);
+	void	sortSequence();
 	void	printContainer();
 };
 
